528b2d7c6c23769d650b1333d9581e99f81ee0dd
Gabriel Benjamin <95311293+gbenjaminsc@users.noreply.github.com>
2024-02-01 14:55:51 -0300

    build: add initial structure with basic folders and files

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
-import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
-import { theme } from "./styles/theme";
+import { AppProvider } from "./contexts";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
