4120585bec6cf32634e183efc7cd022abb63dfcf
Gabriel Benjamin <95311293+gbenjaminsc@users.noreply.github.com>
2024-03-05 21:45:59 -0300

    build: all details for input component

--- src/components/form/input.tsx
+++ src/components/form/input.tsx
@@ -7,20 +7,48 @@ import {
   InputLeftElement,
   InputGroup,
 } from "@chakra-ui/react";
-import { useState, useEffect, useCallback, useRef } from "react";
+
 import { FaExclamation } from "react-icons/fa";
 
-export const Input = () => {
+import { useState, useEffect, useCallback, useRef } from "react";
+import { IconType } from "react-icons";
+import { FieldError } from "react-hook-form";
+
+interface InputProps extends ChakraInputProps {
+  name: string;
+  label?: string;
+  error?: FieldError | null;
+  icon?: IconType;
+}
+
+export const Input = ({
+  name,
+  error = null,
+  icon: Icon,
+  label,
+  ...rest
+}: InputProps) => {
   return (
     <FormControl>
-      <FormLabel>Label</FormLabel>
+      {!!label && <FormLabel>{label}</FormLabel>}
       <InputGroup flexDirection="column">
-        <InputLeftElement mt="2.5">
-          <FaExclamation />
-        </InputLeftElement>
-        <ChakraInput placeholder="Email" />
+        {Icon && (
+          <InputLeftElement mt="2.5">
+            <Icon />
+          </InputLeftElement>
+        )}
+        <ChakraInput
+          name={name}
+          bg="gray.50"
+          variant="outline"
+          _hover={{ bgColor: "gray.100" }}
+          _placeholder={{ color: "gray.300" }}
+          size="lg"
+          h="60px"
+          {...rest}
+        />
 
-        <FormErrorMessage>Erro</FormErrorMessage>
+        {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
       </InputGroup>
     </FormControl>
   );
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,52 +1,59 @@
-import {
-  Flex,
-  Grid,
-  Heading,
-  Image,
-  Input,
-  Text,
-  VStack,
-} from "@chakra-ui/react";
+import { Flex, Grid, Heading, Image, Text, VStack } from "@chakra-ui/react";
 import LogoSecondary from "../../assets/main-logo.svg";
+import { Input } from "../../components/form/input";
+import { useForm } from "react-hook-form";
+import { FaEnvelope, FaLock } from "react-icons/fa";
 
-export const Login = () => (
-  <Flex
-    padding="10px 15px"
-    alignItems="center"
-    height="100vh"
-    bgGradient="linear(to-r, purple.800 65%, white 35% )"
-    color="white"
-  >
+export const Login = () => {
+  const {
+    formState: { errors },
+    register,
+    handleSubmit,
+  } = useForm();
+
+  return (
     <Flex
-      w="100%"
-      justifyContent="center"
-      flexDirection="row"
+      padding="10px 15px"
       alignItems="center"
+      height="100vh"
+      bgGradient="linear(to-r, purple.800 65%, white 35% )"
+      color="white"
     >
-      <Grid w="100%" paddingRight="100px">
-        <Image src={LogoSecondary} alt="doit" boxSize="120px" />
-        <Heading as="h1">O jeito fácil, grátis</Heading>
-        <Text>
-          Flexivel e atrativo de gerenciar
-          <b>seus projetos em uma unica plataforma</b>
-        </Text>
-      </Grid>
-      <Grid
-        as="form"
-        mt="4"
+      <Flex
         w="100%"
-        padding="30px 15px"
-        border="3px solid"
-        borderColor="gray.100"
-        bg="white"
-        color="gray.900"
+        justifyContent="center"
+        flexDirection="row"
+        alignItems="center"
       >
-        <Heading size="lg">Bem vindo de volta!</Heading>
-        <VStack spacing="5">
-          <Input placeholder="Digite seu email" />
-          <Input placeholder="Digite sua senha" />
-        </VStack>
-      </Grid>
+        <Grid w="100%" paddingRight="100px">
+          <Image src={LogoSecondary} alt="doit" boxSize="120px" />
+          <Heading as="h1">O jeito fácil, grátis</Heading>
+          <Text>
+            Flexivel e atrativo de gerenciar
+            <b>seus projetos em uma unica plataforma</b>
+          </Text>
+        </Grid>
+        <Grid
+          as="form"
+          mt="4"
+          w="100%"
+          padding="30px 15px"
+          border="3px solid"
+          borderColor="gray.100"
+          bg="white"
+          color="gray.900"
+        >
+          <Heading size="lg">Bem vindo de volta!</Heading>
+          <VStack mt="6" spacing="5">
+            <Input
+              placeholder="Digite seu login"
+              icon={FaEnvelope}
+              name="email"
+            />
+            <Input placeholder="Digite sua senha" icon={FaLock} {...register("password")} />
+          </VStack>
+        </Grid>
+      </Flex>
     </Flex>
-  </Flex>
-);
+  );
+};
