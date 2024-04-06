c3957ec6fa2ff46aa80dda51a5d0bc78b9952325
Gabriel Benjamin <95311293+gbenjaminsc@users.noreply.github.com>
2024-04-06 10:32:44 -0300

    build: create the task context

---
+++ src/contexts/TaskContext.tsx
@@ -0,0 +1,61 @@
+import {
+  ReactNode,
+  createContext,
+  useCallback,
+  useContext,
+  useState,
+} from "react";
+import { api } from "../services/api";
+import { AxiosResponse } from "axios";
+
+interface TaskProviderProps {
+  children: ReactNode;
+}
+
+interface Task {
+  id: string;
+  title: string;
+  description: string;
+  userId: string;
+  completed: boolean;
+}
+
+interface TaskContextData {
+  tasks: Task[];
+  createTask: (data: Task, accessToken: string) => Promise<void>;
+}
+
+const TaskContext = createContext<TaskContextData>({} as TaskContextData);
+
+const useTasks = () => {
+  const context = useContext(TaskContext);
+
+  if (!context) {
+    throw new Error("useTasks must be used within an TaskProvider");
+  }
+
+  return context;
+};
+
+const TaskProvider = ({ children }: TaskProviderProps) => {
+  const [tasks, setTask] = useState<Task[]>([]);
+
+  const createTask = useCallback(async (data: Task, accessToken: string) => {
+    await api
+      .post("/tasks", data, {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      })
+      .then((response: AxiosResponse<Task>) =>
+        setTask((oldTasks) => [...oldTasks, response.data])
+      )
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <TaskContext.Provider value={{ tasks, createTask }}>
+      {children}
+    </TaskContext.Provider>
+  );
+};
+
+export { useTasks, TaskProvider };
