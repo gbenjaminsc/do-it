59bb45bd090439d2934b760adf27872bc68aadda
Gabriel Benjamin <95311293+gbenjaminsc@users.noreply.github.com>
2024-05-22 21:29:38 -0300

    feat: change the infos to dash from state to the database

--- src/contexts/TaskContext.tsx
+++ src/contexts/TaskContext.tsx
@@ -23,6 +23,7 @@ interface Task {
 interface TaskContextData {
   tasks: Task[];
   createTask: (data: Omit<Task, "id">, accessToken: string) => Promise<void>;
+  loadTasks: (userId: string, accessToken: string) => Promise<void>;
 }
 
 const TaskContext = createContext<TaskContextData>({} as TaskContextData);
@@ -40,6 +41,19 @@ const useTasks = () => {
 const TaskProvider = ({ children }: TaskProviderProps) => {
   const [tasks, setTask] = useState<Task[]>([]);
 
+  const loadTasks = useCallback(async (userId: string, accessToken: string) => {
+    try {
+      const response = await api.get(`/tasks?userId=${userId}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      setTask(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
   const createTask = useCallback(
     async (data: Omit<Task, "id">, accessToken: string) => {
       await api
@@ -55,7 +69,7 @@ const TaskProvider = ({ children }: TaskProviderProps) => {
   );
 
   return (
-    <TaskContext.Provider value={{ tasks, createTask }}>
+    <TaskContext.Provider value={{ tasks, createTask, loadTasks }}>
       {children}
     </TaskContext.Provider>
   );
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,20 +1,33 @@
 import { Box, Grid } from "@chakra-ui/react";
-import { useAuth } from "../../contexts/AuthContext";
 import { Header } from "../../components/Header";
 import { SearchBox } from "../../components/Form/SearchBox";
 import { Card } from "../../components/Card";
+import { useTasks } from "../../contexts/TaskContext";
+import { useAuth } from "../../contexts/AuthContext";
+import { useEffect, useState } from "react";
 
 export const Dashboard = () => {
-  const { signOut } = useAuth();
+  const [loading, setLoading] = useState(true);
+  const { user, accessToken } = useAuth();
+  const { tasks, loadTasks } = useTasks();
+
+  useEffect(() => {
+    loadTasks(user.id, accessToken).then((res) => setLoading(false));
+  }, []);
 
   return (
     <Box>
       <Header />
       <SearchBox />
-      <Grid w="100%" templateColumns="repeat(auto-fill, minmax(420px, 1fr))"
-      gap={10} paddingX="8" mt="8">
-        {[1, 2, 3, 4, 5, 6].map((_) => (
-          <Card />
+      <Grid
+        w="100%"
+        templateColumns="repeat(auto-fill, minmax(420px, 1fr))"
+        gap={10}
+        paddingX="8"
+        mt="8"
+      >
+        {tasks.map((task) => (
+          <Card task={task} />
         ))}
       </Grid>
     </Box>
