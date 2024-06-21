3474476ff4a48f8370912ffd0e7a926b9a79ee61
Gabriel Benjamin <95311293+gbenjaminsc@users.noreply.github.com>
2024-06-21 18:48:42 -0300

    feat: finish the error page and fix layout bug of the nav

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,17 @@ import { Login } from "../pages/login";
 import { Dashboard } from "../pages/dashboard";
 import { Route } from "./route";
 import { SignUp } from "../pages/signup";
+import { PageNotFound } from "../pages/Err";
+import { useAuth } from "../contexts/AuthContext";
 
-export const Routes = () => (
-  <Switch>
-    <Route exact path="/" component={Login} />
-    <Route path="/dashboard" component={Dashboard} isPrivate />
-    <Route path="/signup" component={SignUp} />
-  </Switch>
-);
+export const Routes = () => {
+  const { accessToken } = useAuth();
+  return (
+    <Switch>
+      <Route exact path="/" component={Login} />
+      <Route path="/signup" component={SignUp} />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route component={PageNotFound} isPrivate={!!accessToken} />
+    </Switch>
+  );
+};
